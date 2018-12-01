8d1cc96e2f86e1bdd72d900ebbd1096daaee273a
kkyung0321 <41039867+kkyung0321@users.noreply.github.com>
2018-12-01 17:02:27 +0900

    Update getCursor.c

--- getCursor.c
+++ getCursor.c
@@ -4,15 +4,16 @@
 typedef struct cursor{
 	int x;
 	int y;
-}cur;
-
+}cur; 
+//
 cur getCursor(void){
 
 	initscr();
 	cur temp;
 //	move(10,20);
 	
-	getyx(stdscr,temp.x,temp.y);
+	getyx(stdscr,temp.x,temp.y); //STDSCR 상태 커서를 받을 수있음(real screen 에 나오기 전에 buffer 상태)
+					
 //	printf("%d %d\n", temp.x, temp.y);
 	endwin();
 
