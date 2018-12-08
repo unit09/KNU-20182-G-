c03ad7d13d6ba1dd922fa4935c83309a275e34c7
Jisoo <45275382+unit09@users.noreply.github.com>
2018-12-08 19:38:27 +0900

    Update board.c
    수업시간에 thread내용을 배우면서 게임같은 경우 애니메이션을 그려주는 스레드를 만들어서 하는게 좋다고 해서 교수님께 여쭤보니 교수님도 그게 알람을 이용해서 구현하는거보다 더 좋을 수 있다하여 화면 그리는 부분을 스레드를 만들어서 처리하는 것으로 수정함

--- board.c
+++ board.c
@@ -4,6 +4,9 @@
 #include <time.h>
 #include <unistd.h>
 #include <signal.h>
+#include <sys/time.h>
+#include <pthread.h>
+#include <termios.h>
 
 //board size
 #define BD_H 18
@@ -15,22 +18,27 @@
 #define BLOCK_X 13
 #define BLOCK_Y 0
 
-#define DELAY 1
+#define DELAY 500
 
 int board[BD_H][BD_W] = {0, };
 int score = 0;
 
+void *screen(void *);
 void showboard();
 int createBlock();
 void moveBlock(int, int, int, int);
 int rotation(int);
 
+int ro_x, ro_y, num, shape = 0;
+
 int main(){
 	int i, j;
-	int ro_x, ro_y, num, shape = 0;
-
+	char c;
+	pthread_t screen_thread;
+	
 	signal(SIGQUIT, SIG_IGN);
 	signal(SIGTSTP, SIG_IGN);
+	signal(SIGINT, SIG_IGN);
 
 	for(i = 0; i < BD_H; i++){
 		for(j = 0; j < BD_W; j++){
@@ -46,27 +54,54 @@ int main(){
 		}
 	}
 
+	if(pthread_create(&screen_thread, NULL, screen, (void*)NULL)){
+		perror("pthread_create");
+		exit(1);
+	}
+	
+
 	ro_x = BLOCK_X;
 	ro_y = BLOCK_Y;
-	num = createBlock();
+	num = createBlock();	//나중에 다른곳에도 추가되어야 함
 
+	while(1){	// 모든 행동들이 한타임 느림, 행동을 했을때 즉시 적용되서 보이게 해야됨
+		c = getchar();
+		if(c == 'Q') break;
+		switch(c){
+			case 'f':
+				shape = rotation(shape);
+				break;
+			case 'a':
+				ro_x--;
+				break;
+			case 'd':
+				ro_x++;
+				break;
+			case 's':
+				ro_y++;	//되기는 하는데 너무 빠름 보이지 않을정도로...
+				break;
+		}
+	}
+
+	endwin();
+}
+
+void *screen(void *no){
 	initscr();
+	crmode();
+	noecho();
 
 	while(1){
 		clear();
-
 		showboard();
 		moveBlock(ro_x, ro_y, num, shape);
 
 		move(LINES-1, COLS-1);
 
 		refresh();
-		sleep(DELAY);
+		usleep(DELAY*1000);
 		ro_y++;
-		shape = rotation(shape);
 	}
-
-	endwin();
 }
 
 void showboard(){
@@ -94,7 +129,7 @@ void showboard(){
 	printw("SCORE : %d", score);
 	
 	move(13, 40);
-	printw("QUIT : ctrl + c");
+	printw("QUIT : Q");
 }
 
 int rotation(int shape){
