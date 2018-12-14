de31edda38acfaa373d98bbc41e84efdcc9d6b38
fostudy <fostudy@naver.com>
2018-12-14 21:00:56 +0900

    라인 함수

---
+++ line.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "tetris.h"
+
+void removeLine() {
+	int i, j;
+	int check = 0;
+
+
+	for (i = 0; i < BD_H - 1; i++) {
+		for (j = 1; j < BD_W - 1; j++) {
+			if (board[i][j] == 1) check++;
+		}
+		if (check == BD_W - 2) {
+			for (j = 1; j < BD_W - 1; j++) {
+				board[i][j] = 0;
+			}
+			downLine(i);
+		}
+		check = 0;
+	}
+}
+
+void downLine(int col) {
+	int i, j;
+
+	for (i = col; i > 0; i--) {
+		for (j = 1; j < BD_W - 1; j++) {
+			board[i][j] = board[i - 1][j];
+		}
+	}
+
+	for (j = 1; j < BD_W - 1; j++) {
+		board[0][j] = 0;
+	}
+}
