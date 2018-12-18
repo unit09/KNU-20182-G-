67c26a8b3e7921d7bb51d130feae48c65e461cbc
Jisoo <45275382+unit09@users.noreply.github.com>
2018-12-18 14:36:44 +0900

    Update scores.c

--- scores.c
+++ scores.c
@@ -3,27 +3,44 @@
 int score = 0;
 int level = 1;
 
-int scoreEvent(int gain) {
+void showScore(){
+	move(4, 40);
+	printw("LEVEL : %d", level);
+
+	move(6, 40);
+	printw("SCORE : %d", score);
+	
+	move(8, 40);
+	printw("QUIT : Q");
+
+	move(10, 40);
+	printw("PAUSE : p");
+
+	move(12, 40);
+	printw("RESTART : R");
+}
+
+int scoreEvent(int gain){
 	int i;
 	int delay;
 
-	if (gain == -1) {
+	if(gain == -1){
 		level = 1;
 		score = 0;
 		return 500;
 	}
 
-	for (i = 1; i <= gain; i++)
+	for(i = 1; i <= gain; i++)
 		score += i;
-	if (score >= 5)
+	if(score >= 5)
 		level = 2;
-	if (score >= 10)
+	if(score >= 10)
 		level = 3;
-	if (score >= 20)
+	if(score >= 20)
 		level = 4;
-	if (score >= 30)
+	if(score >= 30)
 		level = 5;
 
-	delay = 600 - (level * 100);
+	delay = 600 - (level*100);
 	return delay;
 }
