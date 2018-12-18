4a1b600b37fb6329956e09d1f0af0a8fc5dfc1a8
Jisoo <45275382+unit09@users.noreply.github.com>
2018-12-18 14:40:45 +0900

    Update line.c

--- line.c
+++ line.c
@@ -4,35 +4,40 @@
 
 //라인 함수
 
-void removeLine() {
+void removeLine(){
 	int i, j;
 	int check = 0;
+	int gain = 0;
+	int delay2;
 
-
-	for (i = 0; i < BD_H - 1; i++) {
-		for (j = 1; j < BD_W - 1; j++) {
-			if (board[i][j] == 1) check++;
+	for(i = 0; i < BD_H-1; i++){
+		for(j = 1; j < BD_W-1; j++){
+			if(board[i][j] == 1) check++;
 		}
-		if (check == BD_W - 2) {
-			for (j = 1; j < BD_W - 1; j++) {
+		if(check == BD_W-2){
+			for(j = 1; j < BD_W-1; j++){
 				board[i][j] = 0;
 			}
+			gain++;
 			downLine(i);
 		}
-		check = 0;
+		check = 0;	
 	}
+	delay2 = scoreEvent(gain);
+	if(delay2 != delay)
+		set_ticker(delay2);
 }
 
-void downLine(int col) {
+void downLine(int col){
 	int i, j;
 
-	for (i = col; i > 0; i--) {
-		for (j = 1; j < BD_W - 1; j++) {
-			board[i][j] = board[i - 1][j];
+	for(i = col; i > 0; i--){
+		for(j = 1; j < BD_W-1; j++ ){
+			board[i][j] = board[i-1][j];
 		}
 	}
 
-	for (j = 1; j < BD_W - 1; j++) {
+	for(j = 1; j < BD_W-1; j++){
 		board[0][j] = 0;
 	}
 }
