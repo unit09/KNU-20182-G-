518462ba93673fc5f256ab6a5b4fe634d656c6bc
Jisoo <45275382+unit09@users.noreply.github.com>
2018-12-18 14:45:27 +0900

    Update tetris.h

--- tetris.h
+++ tetris.h
@@ -13,13 +13,18 @@
 #define BD_Y 4
 //block initial position
 #define BLOCK_X 13
-#define BLOCK_Y 0
+#define BLOCK_Y 1
 
 typedef struct cursor{
 	int x;
 	int y;
 }cur; 
 
-cur getCursor(void);
 int createBlock();
 void paintBlock(cur, int, int);
+void getBlock(int[][4], int, int);
+cur getCursor(cur);
+int rotation(int, int);
+int vaildMove(int[][4], int, int);
+int scoreEvent(int);
+void showScore();
